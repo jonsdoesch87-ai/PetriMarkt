0365c8723f7654cca0f1aed7f18aa4636a83004a
jonsdoesch87-ai <jonsd.oesch87@gmail.com>
2025-12-24 11:23:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App"; // Der Pfad muss genau stimmen
+import "./styles/index.css";
+import App from "./App"; // Importiere die Hauptkomponente
 
 ReactDOM.render(
   <React.StrictMode>
