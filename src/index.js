3606a8dca607c3d4aa9e6aca81f86313e0b94ec5
jonsdoesch87-ai <jonsd.oesch87@gmail.com>
2025-12-24 11:18:04 +0100

    Fix import path for App component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App.js"; // Der Pfad muss genau stimmen
+import App from "./App"; // Der Pfad muss genau stimmen
 
 ReactDOM.render(
   <React.StrictMode>
